f749bf73b714d802010774d415f297634a114e35
Mikhail Malygin <mihail.malygin@gmail.com>
2020-08-20 11:15:59 +0500

    adding the first Page Objects: base_page.py and main_page.py Also adding the first test: test_main_page.py

--- test_main_page.py
+++ test_main_page.py
@@ -1,8 +1,10 @@
+from .pages.main_page import MainPage
 import time
 
+
 def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
-    login_link = browser.find_element_by_css_selector("#login_link")
-    login_link.click()
+    url = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, url)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    page.open()  # открываем страницу
+    page.go_to_login_page()
     time.sleep(10)
